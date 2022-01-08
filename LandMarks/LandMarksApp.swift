673bc5b9bf42bd4098964e14f0f1f45fe7e6683c
Modamori Oluwayomi <77457879+Portia-dot@users.noreply.github.com>
2022-01-07 22:09:37 -0800

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by Oluwayomi M on 2022-01-07.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
